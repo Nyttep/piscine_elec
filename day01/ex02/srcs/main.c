164a49b99018dc7593d5d32f0c70e8060ea9334d
Paul DUBOIS <pdubois@made-f0Ar11s9.clusters.42paris.fr>
2024-04-16 17:04:02 +0200

    day01/ex02

--- day01/ex02/srcs/main.c
+++ day01/ex02/srcs/main.c
@@ -17,19 +17,21 @@ int main()
 
 	PRR &= ~(1 << PRTIM1); //setting PRTIM1 to 0 to enable timer/counters1
 
-	//sets the timer as mode 15 (compare mode) (p131-132 & p140)
-	TCCR1A |= (1 << WGM11) | (1 << WGM10);
+	//sets the timer as mode 14 (compare mode) (p131-132 & p140)
+	TCCR1A |= (1 << WGM11);
+	TCCR1A &= ~(1 << WGM10);
 	TCCR1B |= (1 << WGM12) | (1 << WGM13);
 
 	//sets prescaler divisor of the clock at 1024(p143)
 	TCCR1B |= (1 << CS12) | (1 << CS10);
 	TCCR1B &= ~(1 << CS11);
 
-	//toggles the OC1A pin, here a led (p140)
-	TCCR1A |= (1 << COM1A0) | (1 << COM1B0);
-	TCCR1A &= ~(1 << COM1A1) | ~(1 << COM1B1);
+	//stes led when timer == 0 and turn it off when timer == OCR1A (p140)
+	TCCR1A &= ~(1 << COM1A0);
+	TCCR1A |= (1 << COM1A1);
 
-	OCR1A = 16000000 / 2048; //setting max value of timer (p131-132)
+	ICR1 = 16000000 / 1024; //setting max value of timer to 16000000 / 2048(p131-132)
+	OCR1A = ICR1 / 10; 
 	
 	while (1)
 	{
