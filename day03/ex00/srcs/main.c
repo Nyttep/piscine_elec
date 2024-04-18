7233a4c4be304cb9605400b6841db0b95349fefb
Paul DUBOIS <pdubois@made-f0Ar11s8.clusters.42paris.fr>
2024-04-18 15:11:34 +0200

    fixed baudrate + created func for sending

--- day03/ex00/srcs/main.c
+++ day03/ex00/srcs/main.c
@@ -1,18 +1,24 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-ISR(TIMER1_COMPA_vect) //interrupt function when OCR1A compare match in timer1
+void	uart_tx(char c)
 {
 	while (!(UCSR0A & (1 << UDRE0)));//wait for empty transmit buffer
-	UDR0 = 'Z';//send character
+	UDR0 = c;//send character
+}
+
+ISR(TIMER1_COMPA_vect) //interrupt function when OCR1A compare match in timer1
+{
+	uart_tx('Z');
 }
 
 void	timer1_init()
 {
 	//TIMER1 CONFIG
-	//sets the timer as mode 15 (compare mode FAST PWN OCR1A) (p131-132 & p140)
-	TCCR1A |= (1 << WGM11) | (1 << WGM10);
-	TCCR1B |= (1 << WGM12) | (1 << WGM13);
+	//sets the timer as mode 4 (compare mode CTC OCR1A) (p131-132 & p141)
+	TCCR1A &= ~(1 << WGM11) & ~(1 << WGM10);
+	TCCR1B &= ~(1 << WGM13);
+	TCCR1B |= (1 << WGM12);
 
 	//sets prescaler divisor of the timer1 at 1024(p143)
 	TCCR1B |= (1 << CS12) | (1 << CS10);
@@ -21,9 +27,9 @@ void	timer1_init()
 	//overflow on OCR1A (p140)
 	TCCR1A &= ~(1 << COM1A0) & ~(1 << COM1A1);
 
-	TIMSK1 |= (1 << OCIE1A);//enabling interrupt 
+	TIMSK1 |= (1 << OCIE1A);//enabling interrupt on overflow of timer1
 
-	OCR1A = 16000000 / 1024; //setting max value of timer1 to (16000000 / 1024) to get 1Hz(p131-132)
+	OCR1A = (16000000 / (1024 - 1)); //setting max value of timer1 to (16000000 / (1024 - 1) to get 1Hz(p131-132)
 	
 }
 
@@ -33,11 +39,12 @@ void	uart_init()
 
 	UCSR0C &= ~(1 << UMSEL00) & ~(1 << UMSEL01);// setting aynchronous mode
 	UCSR0A |= (1 << U2X0);//setting clock as double speed
-	UBRR0 = 16;//setting UBRR0 to have a baudrate of 115200
+	//setting UBRR0 to have a baudrate of 115200
+	UBRR0L = (uint8_t)(16 & 0xFF);
+	UBRR0H = (uint8_t)(16 >> 8);
 
-	//setting parity mode to even
-	UCSR0C |= (1 << UPM01);
-	UCSR0C &= ~(1 << UPM00);
+	//setting parity mode to no parity
+	UCSR0C &= ~(1 << UPM00) & ~(1 << UPM01);
 
 	//setting character size as 8
 	UCSR0B &= ~(1 << UCSZ02);
@@ -52,7 +59,7 @@ int main()
 {
 	uart_init();
 	timer1_init();
-	sei();
+	sei();//enable global interrupt
 
 	while (1)
 	{
